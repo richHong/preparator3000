19a2b3b8d0b8dbc64869125ae09a84b2c8d5d420
RichHong <rich.hong@gmail.com>
2016-10-06 20:27:27 -0700

    min stack done

--- problems/stacksAndQueues/prompts/minStack.js
+++ problems/stacksAndQueues/prompts/minStack.js
@@ -9,26 +9,42 @@
 
 class MinStack {
   constructor() {
-    // TODO: Implement constructor
+    this.storage = [];
+    this.minimum = [];
   }
 
   // Push will add an element to the stack
-  push() {
+  push(val) {
     // TODO: Implement push method
+    this.storage.push(val);
+
+    if(this.minimum.length === 0){
+      this.minimum.push(val);
+    }
+    if (val < this.minimum[0]){
+      this.minimum.unshift(val);
+    }
   }
 
   // Pop will remove an element from the stack and return that element
   pop() {
     // TODO: Implement pop method
+    var removed = this.storage.pop();
+    if (removed === this.minimum){
+      this.minimum.shift();
+    }
+    return removed;
   }
 
   // Size returns the size of the stack
   size() {
     // TODO: Implement size method
+    return this.storage.length;
   }
 
   getMin() { // Returns the mininum value in constant time
     // TODO: Implement the getMin method
+   return this.minimum[0];
   }
 }
 
